e793ec4b050b1ad13d45c223d73a593244b221e1
renaud-ninauve <renaud.ninauve.92@gmail.com>
2020-03-16 18:44:42 +0100

    [green] Refacto to use a loop instead of if

--- src/main/java/fr/ninauve/renaud/kata/romannumerals/RomanNumerals.java
+++ src/main/java/fr/ninauve/renaud/kata/romannumerals/RomanNumerals.java
@@ -1,26 +1,72 @@
 package fr.ninauve.renaud.kata.romannumerals;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public final class RomanNumerals {
 
+    private static List<PartialToRomanConverter> partialToRomanConverters = new PartialToRomanConverter.PartialToRomanConvertersBuilder()
+            .partialToRomanConverter(
+                    n -> n < 1 || n > 3000,
+                    n -> {
+                        throw new IllegalArgumentException();
+                    })
+            .partialToRomanConverter(
+                    n -> n <= 3,
+                    n -> IntStream.rangeClosed(1, n)
+                            .mapToObj(i -> "I")
+                            .collect(Collectors.joining()))
+            .partialToRomanConverter(
+                    n -> n == 4,
+                    n -> "IV")
+            .partialToRomanConverter(
+                    n -> n == 5,
+                    n -> "V")
+            .build();
+
     public static String numberToRoman(final int number) {
 
-        if (number < 1 || number > 3000) {
-            throw new IllegalArgumentException();
+        return partialToRomanConverters.stream()
+                .filter(c -> c.isApplicableTo(number))
+                .findFirst()
+                .map(c -> c.convert(number))
+                .orElse(null);
+    }
+
+    private static class PartialToRomanConverter {
+        private final Predicate<Integer> applicableTo;
+        private final Function<Integer, String> convert;
+
+        private static class PartialToRomanConvertersBuilder {
+            private final List<PartialToRomanConverter> partialToRomanConverters = new ArrayList<>();
+
+            private PartialToRomanConvertersBuilder partialToRomanConverter(Predicate<Integer> applicableTo,
+                                                                            Function<Integer, String> convert) {
+                this.partialToRomanConverters.add(new PartialToRomanConverter(applicableTo, convert));
+                return this;
+            }
+
+            private List<PartialToRomanConverter> build() {
+                return partialToRomanConverters;
+            }
         }
-        if (number <= 3) {
-            return IntStream.rangeClosed(1, number)
-                    .mapToObj(n -> "I")
-                    .collect(Collectors.joining());
+
+        private PartialToRomanConverter(Predicate<Integer> applicableTo,
+                                        Function<Integer, String> convert) {
+            this.applicableTo = applicableTo;
+            this.convert = convert;
         }
-        if (number == 4) {
-            return "IV";
+
+        public boolean isApplicableTo(int number) {
+            return applicableTo.test(number);
         }
-        if (number == 5) {
-            return "V";
+
+        public String convert(int number) {
+            return convert.apply(number);
         }
-        return null;
     }
 }
