629f32f01fd24bd2d4c6cd244922e95e7493e2f8
renaud-ninauve <renaud.ninauve.92@gmail.com>
2020-03-16 17:48:11 +0100

    [green] Validate numbers

--- src/main/java/fr/ninauve/renaud/kata/romannumerals/RomanNumerals.java
+++ src/main/java/fr/ninauve/renaud/kata/romannumerals/RomanNumerals.java
@@ -4,6 +4,9 @@ public final class RomanNumerals {
 
     public static String numberToRoman(final int number) {
 
+        if (number < 1 || number > 3000) {
+            throw new IllegalArgumentException();
+        }
         return null;
     }
 }
