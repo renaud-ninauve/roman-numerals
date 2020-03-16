c48345f77d2439d18bec5042ca58f49a55bd4148
renaud-ninauve <renaud.ninauve.92@gmail.com>
2020-03-16 18:18:22 +0100

    [green] Handle conversion for 4 and 5

--- src/main/java/fr/ninauve/renaud/kata/romannumerals/RomanNumerals.java
+++ src/main/java/fr/ninauve/renaud/kata/romannumerals/RomanNumerals.java
@@ -10,9 +10,17 @@ public final class RomanNumerals {
         if (number < 1 || number > 3000) {
             throw new IllegalArgumentException();
         }
-
-        return IntStream.rangeClosed(1, number)
-                .mapToObj(n -> "I")
-                .collect(Collectors.joining());
+        if (number <= 3) {
+            return IntStream.rangeClosed(1, number)
+                    .mapToObj(n -> "I")
+                    .collect(Collectors.joining());
+        }
+        if (number == 4) {
+            return "IV";
+        }
+        if (number == 5) {
+            return "V";
+        }
+        return null;
     }
 }
