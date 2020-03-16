a9aa976608694f68423b8ea1a6832d4d5f208587
renaud-ninauve <renaud.ninauve.92@gmail.com>
2020-03-16 17:43:52 +0100

    [red] Add missing files

--- src/test/java/fr/ninauve/renaud/kata/romannumerals/RomanNumeralsTest.java
+++ src/test/java/fr/ninauve/renaud/kata/romannumerals/RomanNumeralsTest.java
@@ -1,7 +1,25 @@
 package fr.ninauve.renaud.kata.romannumerals;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RomanNumeralsTest {
 
+    @DisplayName("Valid numbers are in the range 1 to 3000.")
+    @ParameterizedTest(name = "number {0} is valid.")
+    @ValueSource(ints = {1, 500, 3000})
+    public void valid_number(int number) {
+        RomanNumerals.numberToRoman(number);
+    }
+
+    @DisplayName("Valid numbers are in the range 1 to 3000.")
+    @ParameterizedTest(name = "number {0} is invalid.")
+    @ValueSource(ints = {-1, 0, 3001})
+    public void invalid_number(int number) {
+        assertThrows(IllegalArgumentException.class,
+                () -> RomanNumerals.numberToRoman(number));
+    }
 }
