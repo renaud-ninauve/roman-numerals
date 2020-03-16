4781fafe1d8821b31bc37394f0fbbbb622354597
renaud-ninauve <renaud.ninauve.92@gmail.com>
2020-03-16 17:58:05 +0100

    [green] Handle roman numbers conversion bellow 3

--- src/main/java/fr/ninauve/renaud/kata/romannumerals/RomanNumerals.java
+++ src/main/java/fr/ninauve/renaud/kata/romannumerals/RomanNumerals.java
@@ -1,5 +1,8 @@
 package fr.ninauve.renaud.kata.romannumerals;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public final class RomanNumerals {
 
     public static String numberToRoman(final int number) {
@@ -7,6 +10,9 @@ public final class RomanNumerals {
         if (number < 1 || number > 3000) {
             throw new IllegalArgumentException();
         }
-        return null;
+
+        return IntStream.rangeClosed(1, number)
+                .mapToObj(n -> "I")
+                .collect(Collectors.joining());
     }
 }
