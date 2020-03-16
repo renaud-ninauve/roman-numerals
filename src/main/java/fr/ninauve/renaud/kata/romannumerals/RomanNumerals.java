2eaf07a9370d6919a361b52d441e73ffe9ddcc3a
renaud-ninauve <renaud.ninauve.92@gmail.com>
2020-03-16 17:42:03 +0100

    [red] Valid numbers tests

---
+++ src/main/java/fr/ninauve/renaud/kata/romannumerals/RomanNumerals.java
@@ -0,0 +1,9 @@
+package fr.ninauve.renaud.kata.romannumerals;
+
+public final class RomanNumerals {
+
+    public static String numberToRoman(final int number) {
+
+        return null;
+    }
+}
---
+++ src/test/java/fr/ninauve/renaud/kata/romannumerals/RomanNumeralsTest.java
@@ -0,0 +1,7 @@
+package fr.ninauve.renaud.kata.romannumerals;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RomanNumeralsTest {
+
+}
